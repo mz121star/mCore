4b02839af96bc691ff64fdebb8600485660c1121
mz121star <mz121star@gmail.com>
2012-11-02 09:44:30 +0800

    added mCore core

---
+++ mCore.image.js
@@ -0,0 +1,42 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: C-Jarrick.Miao
+ * Date: 11/1/12
+ * Time: 12:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+(function (mcore) {
+    mcore.Image = function () {
+        return{
+            preLoadImages:function (arr) {
+                var newimages = [], loadedimages = 0;
+                var postaction = function () {
+                };
+                var arr = (typeof arr != "object") ? [arr] : arr;
+
+                function imageloadpost() {
+                    loadedimages++;
+                    if (loadedimages == arr.length) {
+                        postaction(newimages)
+                    }
+                };
+                for (var i = 0; i < arr.length; i++) {
+                    newimages[i] = new Image();
+                    newimages[i].src = arr[i];
+                    newimages[i].onload = function () {
+                        imageloadpost();
+                    }
+                    newimages[i].onerror = function () {
+                        imageloadpost();
+                    }
+                }
+                return {
+                    done:function (f) {
+                        postaction = f || postaction;
+                    }
+                };
+            }
+        };
+    };
+
+})(mCore);
---
+++ mCore.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: C-Jarrick.Miao
+ * Date: 11/1/12
+ * Time: 12:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+(function () {
+    var mcore = function () {};
+    window.mCore=mcore;
+})();
